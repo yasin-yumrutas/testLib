b2245f571a8e07ab74dfeb0e76ef5f1ef291bce5
yasin-yumrutas <yasinyumrutas0@gmail.com>
2023-12-18 13:57:03 +0300

    Blockchain Library

--- timeLib/timeStruct.go
+++ timeLib/timeStruct.go
@@ -1,7 +1,34 @@
 package TimeLib
 
-type TimeYasin struct {
-	Saniye string
-	Saat   string
-	Dakika int
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+)
+
+type Block struct {
+	Index     int
+	Timestamp string
+	Data      string
+	PrevHash  string
+	Hash      string
+}
+
+func CreateBlock(index int, data string, prevHash string) Block {
+	block := Block{
+		Index:     index,
+		Timestamp: time.Now().String(),
+		Data:      data,
+		PrevHash:  prevHash,
+	}
+	block.Hash = CalculateHash(block)
+	return block
+}
+
+func CalculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + block.Data + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
 }
